7dd6dbf2fcfa5b507739a1d021afb62506adbca3
Wong Pui Lam <alanwong9961@gmail.com>
2021-05-06 18:45:10 +0800

    Added search recipe function

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const port = process.env.PORT || 8080
 
 app.get('/api/v1/recipe', searchRecipe.getAllRecipe);
+app.get('/api/v1/recipe/id/:id', searchRecipe.searchRecipeByID);
+app.get('/api/v1/recipe/name/:name', searchRecipe.searchRecipeByName);
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
--- modules/recipe.js
+++ modules/recipe.js
@@ -3,7 +3,31 @@ const { recipe } = require('./models');
 exports.getAllRecipe = (request, response) => {
     recipe.find({}, (error, recipes) => {
         if (error)
-            console.log(error);
+            return response.status(400).send({
+                message: 'Error'
+            });
+        else 
+            response.send(recipes);
+    });
+};
+
+exports.searchRecipeByID = (request, response) => {
+    recipe.findById(request.params.id, (error, recipes) => {
+        if (error)
+            return response.status(400).send({
+                message: 'ID format error'
+            });
+        else
+            response.send(recipes);
+    });
+};
+
+exports.searchRecipeByName = (request, response) => {
+    recipe.find({ recipeName: new RegExp(request.params.name, "i") }, (error, recipes) => {
+        if (error)
+            return response.status(400).send({
+                message: 'Error'
+            });
         else 
             response.send(recipes);
     });
